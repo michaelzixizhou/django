defcfe93b19e1f0a215f0085ac0c5676f0a1ca87
Michael <michael88zhou@gmail.com>
2022-07-06 02:28:29 -0400

    commit two

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from . import views
+from django.http import HttpResponse
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    print(request.user)
+    return render(request, 'home.html', {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import debug_toolbar
+from myapp import views
 
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
+
 ]
