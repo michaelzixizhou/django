9e458976a334c99f9b45470975b2704214d0b527
Michael <michael88zhou@gmail.com>
2022-07-15 19:24:25 -0400

    background

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
     technology = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='gallery')
+    # image = models.ImageField(upload_to='gallery')
     link = models.URLField(max_length=100, null=True)
     
